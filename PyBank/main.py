c6592a7a90d96c33869946671c865f0173d5d1cb
thallegacy <thallegacy@users.noreply.github.com>
2020-11-15 17:44:48 -0500

    Added average change to PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -14,7 +14,9 @@ with open(pybank_csv, 'r') as csvfile:
     # Set your initial storage for list
     TotalMonthsList = []
     TotalAmountList = []
-  
+    NetChangeList = []
+    previous = 0
+
     # Read and store the header row first (use next to read through data after the header)
     csv_header = next(csvreader)
 
@@ -24,9 +26,17 @@ with open(pybank_csv, 'r') as csvfile:
        #add my elements to my lists
         TotalMonthsList.append(row[0])
         TotalAmountList.append(int(row[1]))
+        
+        #Get the percent change and then add my elements to my lists
+        netchange=int(row[1]) - previous
+        NetChangeList.append(netchange)
+        previous =int(row[1])
     
     total= sum(TotalAmountList)
-
+    NetChangeList.pop(0)
+    averagechange  =sum(NetChangeList)/ len(NetChangeList)
+    
     #print my values
     print(f'Total Months: {len(TotalMonthsList)} ')
     print(f'Total Amount: ${total} ')
+    print(f'Average Change: {round(averagechange,2)} ')
